a0c29216279b42bdb5c08ecb36534f12be191cab
Dgonza19 <Daisy.gu07@gmail.com>
2023-10-31 09:17:39 -0500

    Added html and javacript with css

--- scripts/sample.js
+++ scripts/sample.js
@@ -3,20 +3,20 @@
 window.onload = init;
 
 function init() {
-  const bookingBtn = document.getElementById("bookingBtn");
+  bookingBtn = document.getElementById("bookingBtn");
   bookingBtn.onclick = bookingBtnClicked;
 }
 
-function getRoomRate(checkInDate) {
+function getRoomRate(checkInDate, roomType) {
 
-  const checkInDateConfirmation = new Date(checkInDate);
-  checkInDateConfirmation = new Date(checkInDate).getMonth() + 1;
-  const checkInMonth = checkInDateConfirmation.getMonth() + 1;
+  const checkInDateConfirmation = new Date (checkInDate);
+  const checkInMonth = new Date(checkInDate).getMonth() + 1;
+  const month = checkInDateConfirmation.getMonth() + 1;
   const year = checkInDateConfirmation.getFullYear();
 
-  let season;
+  let season; 
   const currentDate = new Date();
-  const peakSeason = currentMonth >= 5 && currentMonth <= 7;
+  const peakSeason = currentMonth >= 5 && currentMonth <= 7; 
 
   if (checkInMonth >= 6 && checkInMonth <= 8) {
     season = peakSeason;
@@ -25,30 +25,33 @@ function getRoomRate(checkInDate) {
   let roomRate;
   peakSeason = 100.00
   if (twoBdSuite) {
-    peakSeason = 140.00;
+    peakSeason = 140.00
   }
   const queenRoom = 150.00;
   const kingRoom = 150.00;
   const twoBdSuite = 210.00;
 
   if (queenRoom) {
-    roomRate = (queenRoom + peakSeason);
+  roomRate = (queenRoom + peakSeason);
   }
 
-  else if (kingRoom) {
-    roomRate = (kingRoom + peakSeason);
+  if (kingRoom) {
+  roomRate = (kingRoom + peakSeason);
   }
 
-  else if (twoBdSuite) {
-    peakSeason = (twoBdSuite + peakSeason);
+  else (twoBdSuite) {
+  peakSeason = (twoBdSuite + peakSeason);
   }
-  return (roomRate);
+  return(roomRate);
 }
 
 function bookingBtnClicked() {
 
   const inputNumNights = document.getElementById("numNights");
   let numNights = Number(inputNumNights.value);
+
+  const inputNumRooms = document.getElementById("numRooms");
+  let numRooms = Number(inputNumRooms.value);
 
   const inputNumAdults = document.getElementById("numAdults");
   let numAdults = Number(inputNumAdults);
@@ -58,56 +61,56 @@ function bookingBtnClicked() {
 
   const messageDiv = ("The room you selected will not hold your party")
 
-  // Room Type
+    // Room Type
 
-  const queenRoom = document.getElementById('queenRadio');
-  const kingRoom = document.getElementById('kingRadio');
-  const twoBdSuite = document.getElementById('2bdRadio');
+    const queenRoom = document.getElementById('queenRadio');
+    const kingRoom = document.getElementById('kingRadio');
+    const twoBdSuite = document.getElementById('2bdRadio');
 
-  let roomType = 0;
-  if (queenRoom.checked) {
-    roomType = queenRoom;
-  }
-  else if (kingRoom.checked) {
-    roomType = kingRoom;
-  }
-  else if (twoBdSuite.checked) {
-    roomType = twoBdSuite;
-  }
+    let roomType = 0;
+    if (queenRoom.checked) {
+      roomType = queenRoom;
+    }
+    if (kingRoom.checked) {
+      roomType = kingRoom;
+    }
+    else (twoBdSuite.checked) {
+      roomType = twoBdSuite;
+    }
 
-  let maxOccupancy;
-  if (queenRoom.checked) {
-    maxOccupancy <= 5;
-  }
-  if (kingRoom.checked) {
-    maxOccupancy <= 2;
-  }
-  if (twoBdSuite.checked) {
-    maxOccupancy <= 6;
-  }
-  else {
+  const maxOccupancy; {
+    if (queenRoom.checked) {
+      maxOccupancy <= 5;
+    }
+    if (kingRoom.checked) {
+      maxOccupancy <= 2;
+    }
+    if (twoBdSuite.checked) {
+      maxOccupancy <= 6;
+    } 
+    else {
     return (messageDiv);
+    }
   }
 
+    // Discounts 
 
-  // Discounts 
-
-  let discounts;
+    let discounts;
   if (seniorRadio.checked) {
     seniorRadio = .10;
   }
   if (militaryRadio.checked) {
     militaryRadio = .20;
   }
-  else if (noneRadio.checked) {
+  else (noneRadio.checked) {
     noneRadio = 0;
   }
 
   // Total Cost of Stay 
 
   const baseRoomCost = roomRate * numNights;
-  const discount = baseRoomCost * discounts;
-  const afterDiscounts = baseRoomCost - discount;
+  const discount = standardRoomCost * discounts;
+  const afterDiscounts = standardRoomCost - discount;
   const tax = afterDiscounts * .12;
   const totalCost = afterDiscounts + tax;
 
@@ -124,10 +127,10 @@ function bookingBtnClicked() {
   outputTotalCost.innerHTML = "$" + totalCost.toFixed(2);
 
   // Confirmation Number
-  const checkInDateConfirmation = new Date(checkInDate);
+  const checkInDateConfirmation = new Date (checkInDate);
   const month = checkInDateConfirmation.getMonth() + 1;
   const year = checkInDateConfirmation.getFullYear();
-  const confirmationNumber = inputName.substr(0, 3) + "-" + month + year + "-" + numNights + ":" + numAdults + ":" + numChildren;
+  const confirmationNumber = inputName.substr(0, 3) + "-" + month + year + "-" + numNights + ":" + inputNumAdults + ":" + inputNumChildren;
   document.getElementById('confirmationNumber').innerHTML = confirmationNumber;
 }
 
